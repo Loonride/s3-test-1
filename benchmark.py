6eb196696cec7fb07dca4fe921e7678fa5959118
Kirill Nagaitsev <knagaitsev@uchicago.edu>
2021-09-23 17:32:11 -0500

    better mem management for large byte sizes

--- benchmark.py
+++ benchmark.py
@@ -3,6 +3,7 @@ import timeit
 import secrets
 import json
 import os
+import sys
 from statistics import mean
 from pathlib import Path
 from abc import ABC, abstractmethod
@@ -17,19 +18,12 @@ class Benchmark(ABC):
     def __init__(self):
         self.n_tasks = 1000
         self.task_size_bytes = None
-        self.tasks = []
         self.data_filename = 'data.json'
         self.name = 'data'
 
         file_dir = Path(__file__).parent.resolve()
         self.data_dir = os.path.join(file_dir, 'data')
         Path(self.data_dir).mkdir(parents=True, exist_ok=True)
-
-    def init_tasks(self, task_size_bytes):
-        self.task_size_bytes = task_size_bytes
-        self.tasks = []
-        for _ in range(self.n_tasks):
-            self.tasks.append(generate_bytes(task_size_bytes))
 
     def get_data_path(self):
         data_path = os.path.join(self.data_dir, self.data_filename)
@@ -48,7 +42,7 @@ class Benchmark(ABC):
         pass
 
     def time_size(self, task_size_bytes):
-        self.init_tasks(task_size_bytes)
+        self.task_size_bytes = task_size_bytes
         put_ts = self.put_times()
         get_ts = self.get_times()
         delete_ts = self.delete_times()
@@ -61,7 +55,7 @@ class Benchmark(ABC):
         return (put_ts, get_ts, delete_ts)
 
     def time_sizes(self):
-        sizes = [1000, 10000, 100000, 1000000]
+        sizes = [1000 , 10000, 100000, 1000000]
         put_group = {}
         get_group = {}
         delete_group = {}
@@ -92,12 +86,13 @@ class S3Benchmark(Benchmark):
     def put_times(self):
         self.objs = []
         times = []
-        for task in self.tasks:
+        for _ in range(self.n_tasks):
             task_id = str(uuid.uuid4())
             obj = self.s3.Object('kir-test-bucket-1', task_id)
             self.objs.append(obj)
 
-            t = timeit.timeit(lambda: obj.put(Body=task), number=1)
+            b = generate_bytes(self.task_size_bytes)
+            t = timeit.timeit(lambda: obj.put(Body=b), number=1)
             times.append(t)
         return times
 
@@ -126,3 +121,4 @@ class RedisBenchmark(Benchmark):
 if __name__ == '__main__':
     s3_bench = S3Benchmark()
     s3_bench.time_sizes()
+    # redis_bench = RedisBenchmark()
