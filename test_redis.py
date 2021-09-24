4d46a76a9a7b1cc039c55f169d781738472f3e97
Kirill Nagaitsev <knagaitsev@uchicago.edu>
2021-09-23 22:47:57 -0500

    switch from_url to Redis class

--- test_redis.py
+++ test_redis.py
@@ -4,6 +4,6 @@ import redis
 redis_host = os.environ.get('REDIS_HOST')
 print(f"Using redis host: {redis_host}")
 
-r = redis.from_url(redis_host, port=6379, db=0)
+r = redis.Redis(redis_host, port=6379, db=0)
 r.set('foo', 'bar')
 r.get('foo')
