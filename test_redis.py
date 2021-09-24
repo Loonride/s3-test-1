3adf0ada295bdf1985e03b2c021f73a08698ddb2
Kirill Nagaitsev <knagaitsev@uchicago.edu>
2021-09-23 23:26:09 -0500

    modify redis test slightly

--- test_redis.py
+++ test_redis.py
@@ -5,5 +5,8 @@ redis_host = os.environ.get('REDIS_HOST')
 print(f"Using redis host: {redis_host}")
 
 r = redis.Redis(redis_host, port=6379, db=0)
-r.set('foo', 'bar')
+
+r.set('foo', b'bar')
 print(r.get('foo'))
+
+r.delete('foo')
